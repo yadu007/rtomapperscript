a43c2cbbb933409c61a7e874e02863043cc664a6
yadu <yadu@ensurity.in>
2020-03-02 17:36:21 +0530

    asdf

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='rtomapperscript',
+      version='0.1',
+      description='RTO mapping',
+      url='',
+      author='Yadu',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['rtomapperscript'],
+      zip_safe=False)
+
